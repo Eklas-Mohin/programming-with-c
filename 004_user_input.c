23a3a46b41d12cb08a29ae772b09deab5bb0bb59
Eklas-Mohin <mohin030299@gmail.com>
2024-11-20 01:03:38 +0600

    Taking input from user in C

--- 004_user_input.c
+++ 004_user_input.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int age;
-    char grade;
-    float height;
+    int student_id; 
+    char letter_grade;
+    float final_cgpa;
 
     // User input for an integer
-    printf("Enter your age: ");
-    scanf("%d", &age); // %d is used for integers, &age is the address of variable
+    printf("Enter your student id: ");
+    scanf("%d", &student_id); // &student_id is the address of variable
 
     // User input for a single character
     printf("Enter your grade: ");
-    scanf("%c", &grade); // %c is used for single character
+    scanf("%c", &letter_grade); // %c is used for single character
 
     // User input for a floating-point number
-    printf("Enter your height in meters: ");
-    scanf("%f", &height); // &height is the address of variable
+    printf("Enter your final exam cgpa: ");
+    scanf("%f", &final_cgpa); // &final_cgpa is the address of variable
 
     return 0;
 }
