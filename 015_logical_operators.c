67fa1b5ad3c697b781f9596a3c8bfd198d0b5369
Eklas-Mohin <mohin030299@gmail.com>
2024-11-20 10:38:56 +0600

    Logical Operators in C

---
+++ 015_logical_operators.c
@@ -0,0 +1,35 @@
+/*
+    Logical Operators
+        &&   and
+        ||   or
+        !    not
+*/
+
+#include <stdio.h>
+
+int main() {
+    int k;
+    printf("Enter k: ");
+    scanf("%d", &k);
+    if (k >= 18 && k <= 60) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+    if (k % 5 == 0 && k % 7 == 0) {
+        printf("k is divisible by both 5 and 7\n");
+    } 
+    if (k % 5 == 0 || k % 7 == 0) {
+        printf("k is divisible by 5 or 7\n");
+    } 
+    if (!(k % 5 == 0)) {
+        printf("k is not divisible by 5\n");
+    } else {
+        printf("k is divisible by 5\n");
+    }
+    printf("1 && 0 : %d\n", 1 && 0);
+    printf("1 || 0 : %d\n", 1 || 0);
+    printf("!(1 && 0) : %d\n", !(1 && 0));
+    printf("!(1 || 0) : %d\n", !(1 || 0));
+    return 0;
+}
